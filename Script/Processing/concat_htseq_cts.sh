93e64b041942e8e527fb9bed25078653bd1c2c2e
lz711 <68362233+lz711@users.noreply.github.com>
2021-05-18 16:01:55 -0700

    Add files via upload

---
+++ Script/Processing/concat_htseq_cts.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+#ct_dir='/mnt/IPGcrc/IPG04_RNA_test/A01_180731_pipeline_w_fusion'
+ct_dir='.'
+>all.htseq.counts.txt
+
+i=0
+for fn in ${ct_dir}/P*.Aligned.htseq.counts.txt 
+do
+  ((i+=1))
+  basefn=$(basename $fn)
+  sample=$(echo $basefn | awk -F. '{ print $1 }')
+  echo "Processing sample $sample"
+  if [[ $i -eq 1 ]]; then sed "1i Gene\t$sample" $fn | cut -f1 >all.htseq.counts.txt; fi;
+  sed "1i Gene\t$sample" $fn | cut -f2 | paste all.htseq.counts.txt - >tmp.htseq.counts.txt
+  mv tmp.htseq.counts.txt all.htseq.counts.txt
+done
+
+  
